25514d44a253b0723a2defd033f9ea11aebb3b70
SonSmoke <nhoksonoi@gmail.com>
2021-11-10 14:54:24 +0700

    fontend web bida
    check fontend web bida

---
+++ bida web/js/slider.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+    $('.slideshow-container').slick({
+        autoplaySpeed: 3000,
+        autoplay: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        fade: true,
+
+
+
+    });
+
+});

f3c92be9c8b0f72a8921daf5e9f76bb573ab037b
SonSmoke <nhoksonoi@gmail.com>
2021-11-12 20:54:17 +0700

    Initial commit

--- bida web/js/slider.js
+++ bida web/js/slider.js
@@ -1,14 +1,33 @@
 $(document).ready(function() {
     $('.slideshow-container').slick({
-        autoplaySpeed: 3000,
-        autoplay: true,
+        infinite: true,
         slidesToShow: 1,
         slidesToScroll: 1,
-        arrows: false,
-        fade: true,
-
-
-
+        dots: true,
+        centerMode: false,
+        variableWidth: false,
+        autoplay: true,
+        autoplaySpeed: 5000
     });
+
+
+    $('.firms').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+    });
+
+
+
+    const clearInput = () => {
+        const input = document.getElementsByTagName("input")[0];
+        input.value = "";
+    }
+
+    const clearBtn = document.getElementById("clear-btn");
+    clearBtn.addEventListener("click", clearInput);
+
+
 
 });
